058cd1592c9357c626eea6c08d1ef6fab8b304b7
kongxianglei <1032805171@qq.com>
2018-07-26 18:17:53 +0800

    atm - 认证

--- frist-atm/atm/core/main.py
+++ frist-atm/atm/core/main.py
@@ -16,6 +16,27 @@ user_data = {
 #access logger
 access_logger = logger.logger('access')
 
+def interactive(user_data):
+    '''
+    interact with user
+    :return:
+    '''
+    menu = '''
+    \033[32;1m
+    1.  账户信息
+    2.  还款
+    3.  取款
+    4.  转账
+    5.  账单
+    6.  退出
+    \033[0m
+    '''
+    print (menu)
+
+
 def run():
     print ("weclome!!!!")
     acc_data = auth.acc_login(user_data,access_logger)
+    if user_data['is_authebticated']:
+        user_data['account_data'] = acc_data
+        interactive(user_data)
