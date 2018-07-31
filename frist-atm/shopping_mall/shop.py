59d1f60c16d996e0644a34d798abffe07819b51d
kongxianglei <1032805171@qq.com>
2018-07-31 09:44:55 +0800

    shopping

--- frist-atm/shopping_mall/shop.py
+++ frist-atm/shopping_mall/shop.py
@@ -3,11 +3,12 @@
 
 import os
 import sys
-
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(base_dir)
-print (base_dir)
-from core import main
+# sys.path.append('%s\\atm' % base_dir)
+print (sys.path)
+
+from atm.core import main
 
 if __name__ == '__main__':
     main.run()
